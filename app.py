5abe074a52024d03b0c99892ea5d8f1fb1e70d3f
drod75 <dr507498@gmail.com>
2024-12-05 11:51:28 -0500

    hf_space_equality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import numpy as np 
+import numpy as np
 import pandas as pd  # type: ignore
 import os
 import keras
@@ -14,20 +14,24 @@ from langchain.schema.output_parser import StrOutputParser
 from langchain_core.messages import HumanMessage, SystemMessage
 from PIL import Image
 import json
-from streamlit_extras.bottom_container import bottom
+from dotenv import load_dotenv
 
 st.set_page_config(
-    page_title="Food Chain", 
+    page_title="Food Chain",
     page_icon="🍴"
 )
 
+load_dotenv()
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 mongo_uri = os.getenv("MONGO_URI_RAG_RECIPE")
 
+
 @st.cache_resource
 def loadEmbedding():
-    embedding = SentenceTransformer("thenlper/gte-large") 
+    embedding = SentenceTransformer("thenlper/gte-large")
     return embedding
+
+
 embedding = loadEmbedding()
 
 
@@ -49,6 +53,7 @@ def get_mongo_client(mongo_uri):
         print(f"Connection failed: {e}")
         return None
 
+
 if not mongo_uri:
     print("MONGO_URI not set in env")
 
@@ -56,6 +61,7 @@ mongo_client = get_mongo_client(mongo_uri)
 
 mongo_db = mongo_client['recipes']
 mongo_collection = mongo_db['recipesCollection']
+
 
 def vector_search(user_query, collection):
     query_embedding = getEmbedding(user_query)
@@ -96,6 +102,7 @@ def vector_search(user_query, collection):
     pipeline = [vector_search_stage, unset_stage, project_stage]
     results = mongo_collection.aggregate(pipeline)
     return list(results)
+
 
 def mongo_retriever(query):
     documents = vector_search(query, mongo_collection)
@@ -139,12 +146,14 @@ Use the provided context to generate a result based on the following JSON format
 Instructions:
 1. Focus on the user's specific request and avoid irrelevant ingredients or approaches.
 2. Do not return anything other than the JSON.
-3. If the answer is unclear or the context does not fully address the prompt, return []
+3. If the answer is unclear or the context does not fully address the prompt, return [].
 4. Base the response on simple, healthy, and accessible ingredients and techniques.
 5. Rewrite the description in third person
 Context: {context}
 When choosing a recipe from the context, FOLLOW these instructions:
 1. The recipe should be makeable from scratch, using only proper ingredients and not other dishes or pre-made recipes
+2. If the recipes from the context makes sense but do not match {question}, generate an amazing recipe for {question} following the above template.
+3. If context does not match {question} at all, return []
 Question: {question}
 """
 
@@ -163,6 +172,7 @@ rag_chain = (
     | StrOutputParser()
 )
 
+
 def get_response(query):
     return rag_chain.invoke(query)
 
@@ -171,51 +181,56 @@ def get_response(query):
 # Classifier
 img_size = 224
 
+
 @st.cache_resource
 def loadModel():
-    model = load_model('models/efficientnet-fine-d1.keras')
+    model = load_model('efficientnet-fine-d1.keras')
     return model
+
 
 model = loadModel()
 
 
 class_names = [
-    "apple_pie", "baby_back_ribs", "baklava", "beef_carpaccio", "beef_tartare", "beet_salad", 
-    "beignets", "bibimbap", "bread_pudding", "breakfast_burrito", "bruschetta", "caesar_salad", 
-    "cannoli", "caprese_salad", "carrot_cake", "ceviche", "cheese_plate", "cheesecake", "chicken_curry", 
-    "chicken_quesadilla", "chicken_wings", "chocolate_cake", "chocolate_mousse", "churros", "clam_chowder", 
-    "club_sandwich", "crab_cakes", "creme_brulee", "croque_madame", "cup_cakes", "deviled_eggs", "donuts", 
-    "dumplings", "edamame", "eggs_benedict", "escargots", "falafel", "filet_mignon", "fish_and_chips", "foie_gras", 
-    "french_fries", "french_onion_soup", "french_toast", "fried_calamari", "fried_rice", "frozen_yogurt", 
-    "garlic_bread", "gnocchi", "greek_salad", "grilled_cheese_sandwich", "grilled_salmon", "guacamole", "gyoza", 
-    "hamburger", "hot_and_sour_soup", "hot_dog", "huevos_rancheros", "hummus", "ice_cream", "lasagna", 
-    "lobster_bisque", "lobster_roll_sandwich", "macaroni_and_cheese", "macarons", "miso_soup", "mussels", 
-    "nachos", "omelette", "onion_rings", "oysters", "pad_thai", "paella", "pancakes", "panna_cotta", "peking_duck", 
-    "pho", "pizza", "pork_chop", "poutine", "prime_rib", "pulled_pork_sandwich", "ramen", "ravioli", "red_velvet_cake", 
-    "risotto", "samosa", "sashimi", "scallops", "seaweed_salad", "shrimp_and_grits", "spaghetti_bolognese", 
-    "spaghetti_carbonara", "spring_rolls", "steak", "strawberry_shortcake", "sushi", "tacos", "takoyaki", "tiramisu", 
+    "apple_pie", "baby_back_ribs", "baklava", "beef_carpaccio", "beef_tartare", "beet_salad",
+    "beignets", "bibimbap", "bread_pudding", "breakfast_burrito", "bruschetta", "caesar_salad",
+    "cannoli", "caprese_salad", "carrot_cake", "ceviche", "cheese_plate", "cheesecake", "chicken_curry",
+    "chicken_quesadilla", "chicken_wings", "chocolate_cake", "chocolate_mousse", "churros", "clam_chowder",
+    "club_sandwich", "crab_cakes", "creme_brulee", "croque_madame", "cup_cakes", "deviled_eggs", "donuts",
+    "dumplings", "edamame", "eggs_benedict", "escargots", "falafel", "filet_mignon", "fish_and_chips", "foie_gras",
+    "french_fries", "french_onion_soup", "french_toast", "fried_calamari", "fried_rice", "frozen_yogurt",
+    "garlic_bread", "gnocchi", "greek_salad", "grilled_cheese_sandwich", "grilled_salmon", "guacamole", "gyoza",
+    "hamburger", "hot_and_sour_soup", "hot_dog", "huevos_rancheros", "hummus", "ice_cream", "lasagna",
+    "lobster_bisque", "lobster_roll_sandwich", "macaroni_and_cheese", "macarons", "miso_soup", "mussels",
+    "nachos", "omelette", "onion_rings", "oysters", "pad_thai", "paella", "pancakes", "panna_cotta", "peking_duck",
+    "pho", "pizza", "pork_chop", "poutine", "prime_rib", "pulled_pork_sandwich", "ramen", "ravioli", "red_velvet_cake",
+    "risotto", "samosa", "sashimi", "scallops", "seaweed_salad", "shrimp_and_grits", "spaghetti_bolognese",
+    "spaghetti_carbonara", "spring_rolls", "steak", "strawberry_shortcake", "sushi", "tacos", "takoyaki", "tiramisu",
     "tuna_tartare", "waffles"
 ]
+
 
 def classifyImage(input_image):
     input_image = input_image.resize((img_size, img_size))
     input_array = tf.keras.utils.img_to_array(input_image)
 
-    # Add a batch dimension 
+    # Add a batch dimension
     input_array = tf.expand_dims(input_array, 0)  # (1, 224, 224, 3)
-    
+
     predictions = model.predict(input_array)[0]
     print(f"Predictions: {predictions}")
 
     # Sort predictions to get top 5
     top_indices = np.argsort(predictions)[-5:][::-1]
-    
+
     # Prepare the top 5 predictions with their class names and percentages
-    top_predictions = [(class_names[i], predictions[i] * 100) for i in top_indices]
+    top_predictions = [(class_names[i], predictions[i] * 100)
+                       for i in top_indices]
     for i, (class_name, confidence) in enumerate(top_predictions, 1):
         print(f"{i}. Predicted {class_name} with {confidence:.2f}% Confidence")
 
     return top_predictions
+
 
 def capitalize_after_number(input_string):
     # Split the string on the first period
@@ -225,22 +240,28 @@ def capitalize_after_number(input_string):
     return input_string
 ##############################################
 
-#for displaying RAG recipe response
+# for displaying RAG recipe response
+
+
 def display_response(response):
     """
     Function to format a JSON response into Streamlit's `st.write()` format.
     """
+    if response == "[]":
+        st.write("No recipes found :(")
+        return
     if isinstance(response, str):
         # Convert JSON string to dictionary if necessary
         response = json.loads(response)
-    
+
     st.write("### Recipe Details")
     st.write(f"**Name:** {response['name'].capitalize()}")
     st.write(f"**Preparation Time:** {response['minutes']} minutes")
     st.write(f"**Description:** {response['description'].capitalize()}")
     st.write(f"**Tags:** {', '.join(response['tags'])}")
     st.write("### Ingredients")
-    st.write(", ".join([ingredient.capitalize() for ingredient in response['ingredients']]))
+    st.write(", ".join([ingredient.capitalize()
+             for ingredient in response['ingredients']]))
     st.write(f"**Total Ingredients:** {response['n_ingredients']}")
     st.write("### Nutrition Information (per serving)")
     st.write(", ".join(response['formatted_nutrition']))
@@ -248,6 +269,7 @@ def display_response(response):
     st.write("### Steps")
     for step in response['formatted_steps']:
         st.write(capitalize_after_number(step))
+
 
 def display_dishes_in_grid(dishes, cols=3):
     rows = len(dishes) // cols + int(len(dishes) % cols > 0)
@@ -259,23 +281,27 @@ def display_dishes_in_grid(dishes, cols=3):
                 st.sidebar.write(dish.replace("_", " ").capitalize())
 # #Streamlit
 
-#Left sidebar title
+
+# Left sidebar title
 st.sidebar.markdown(
-    "<h1 style='font-size:32px;'>RAG Recipe</h1>", 
+    "<h1 style='font-size:32px;'>RAG Recipe</h1>",
     unsafe_allow_html=True
 )
 
-st.sidebar.write("Upload an image and/or enter a query to get started! Explore our trained dish types listed below for guidance.")
+st.sidebar.write(
+    "Upload an image and/or enter a query to get started! Explore our trained dish types listed below for guidance.")
 
 uploaded_image = st.sidebar.file_uploader("Choose an image:", type="jpg")
 query = st.sidebar.text_area("Enter your query:", height=100)
+recipe_submit = st.sidebar.button(
+    label='Chain Recipe', icon=':material/link:', use_container_width=True)
 
 # gap
 st.sidebar.markdown("<br><br><br>", unsafe_allow_html=True)
 selected_dish = st.sidebar.selectbox(
-    "Search for a dish that our model can classify:", 
+    "Search for a dish that our model can classify:",
     options=class_names,
-    index=0  
+    index=0
 )
 
 # Right title
@@ -290,55 +316,50 @@ with st.expander("**What is FOOD CHAIN?**"):
         We want users to have an easy way to figure out what their favorite foods contain, to know any 
         allergens in the food and to better connect to the food around them. This tool can also tell users 
         the calories of their dish, they can figure out the nutrients with only a few steps!
-
         Thank you for using our project!
+        Made by the Classify Crew: [Contact List](https://linktr.ee/classifycrew)
         """
     )
-
-# bottom
-with bottom():
-    st.markdown("Made by the Classify Crew: [Contact List](https://linktr.ee/classifycrew)")
-
-
 #################
 
-
 # Image Classification Section
-if uploaded_image and query:
+if uploaded_image and recipe_submit:
     with st.expander("**Food Classification**", expanded=True, icon=':material/search_insights:'):
         st.title("Results: Image Classification")
-
         # Open the image
         input_image = Image.open(uploaded_image)
 
         # Display the image
-        st.image(input_image, caption="Uploaded Image.", use_container_width=True)
-            
+        st.image(input_image, caption="Uploaded Image.",
+                 use_container_width=True)
+
         predictions = classifyImage(input_image)
         fpredictions = ""
 
         # Show the top predictions with percentages
-        st.markdown("**Top Predictions:**")
+        st.write("Top Predictions:")
         for class_name, confidence in predictions:
             if int(confidence) > 0.05:
                 fpredictions += f"{class_name}: {confidence:.2f}%,"
-            class_name = class_name.replace("_", " ")
-            class_name = class_name.title()
-            st.markdown(f"**{class_name}**: {confidence:.2f}%")
+            if int(confidence) > 5:
+                class_name = class_name.replace("_", " ")
+                class_name = class_name.title()
+                st.markdown(f"*{class_name}*: {confidence:.2f}%")
         print(fpredictions)
 
     # call openai to pick the best classification result based on query
     openAICall = [
         SystemMessage(
-            content = "You are a helpful assistant that identifies the best match between classified food items and a user's request based on provided classifications and keywords."
+            content="You are a helpful assistant that identifies the best match between classified food items and a user's request based on provided classifications and keywords."
         ),
-        HumanMessage( 
-            content = f"""
+        HumanMessage(
+            content=f"""
                 Based on the following image classification with percentages of each food:
                 {fpredictions}
                 And the following user request:
                 {query}
-                Return to me JUST ONE of the classified images that most relates to the user request, based on the relevance of the user query.
+                Return to me JUST ONE of the classified images that most relates to the user request.
+                If the predictions and the user query do not correlate, return the most likely dish focusing on the user query.
                 in the format: [dish]
             """
         ),
@@ -348,44 +369,41 @@ if uploaded_image and query:
     openAIresponse = llm.invoke(openAICall)
     print("AI CALL RESPONSE: ", openAIresponse.content)
 
-    # RAG the openai response and display
-    print("RAG INPUT", openAIresponse.content + " " + query)
-
+    RAGresponse = get_response(openAIresponse.content + " " + query)
     with st.expander("Recipe Generation", expanded=True, icon=':material/menu_book:'):
         st.title('Results: RAG')
-        RAGresponse = get_response(openAIresponse.content + " " + query)
+        # RAG the openai response and display
+        print("RAG INPUT", openAIresponse.content + " " + query)
         display_response(RAGresponse)
 elif uploaded_image is not None:
     with st.expander("**Food Classification**", expanded=True, icon=':material/search_insights:'):
         st.title("Results: Image Classification")
-
         # Open the image
         input_image = Image.open(uploaded_image)
 
         # Display the image
-        st.image(input_image, caption="Uploaded Image.", use_container_width=True)
+        st.image(input_image, caption="Uploaded Image.", use_column_width=True)
 
         # Classify the image and display the result
         predictions = classifyImage(input_image)
         fpredictions = ""
 
         # Show the top predictions with percentages
-        st.markdown("**Top Predictions:**")
+        st.write("Top Predictions:")
         for class_name, confidence in predictions:
             if int(confidence) > 0.05:
                 fpredictions += f"{class_name}: {confidence:.2f}%,"
             if int(confidence) > 5:
                 class_name = class_name.replace("_", " ")
                 class_name = class_name.title()
-                st.markdown(f"**{class_name}**: {confidence:.2f}%")
+                st.markdown(f"*{class_name}*: {confidence:.2f}%")
         print(fpredictions)
 
-elif query:
+elif recipe_submit:
+    response = get_response(query)
     with st.expander("**Recipe Generation**", expanded=True, icon=':material/menu_book:'):
         st.title("Results: RAG")
-        response = get_response(query)
-        print(response,'\n\n\n\n\n\n\n')
         display_response(response)
 else:
-    st.warning("Please input an image and/or a prompt.", icon=':material/no_meals:')
-
+    st.warning("Please input an image and/or a prompt.",
+               icon=':material/no_meals:')
